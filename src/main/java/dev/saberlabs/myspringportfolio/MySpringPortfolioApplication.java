c796e302716a5d300f30e5773468602d17f69568
devbossma <ysaber201@outlook.com>
2026-02-25 02:33:38 +0000

    Initialization of Spring Boot Investments Portfolio Project

---
+++ src/main/java/dev/saberlabs/myspringportfolio/MySpringPortfolioApplication.java
@@ -0,0 +1,13 @@
+package dev.saberlabs.myspringportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringPortfolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/saberlabs/myspringportfolio/MySpringPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.saberlabs.myspringportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
